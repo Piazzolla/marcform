29996cd9949e88a90cda428be74d709908dc7996
Mariano Burgos <mariano.burgos@pjn.gov.ar>
2016-04-25 17:06:46 -0300

    agrego input de mensajes

--- MarcForm/app/app.js
+++ MarcForm/app/app.js
@@ -6,3 +6,33 @@ var myAppModule = angular.module('marcform', []);
 myAppModule.controller('DemoController', ['$scope', function($scope){
 	$scope.name = 'blah';
 	}]);	
+
+myAppModule.factory('messages', function(){
+  var messages = {};
+
+  messages.list = [];
+
+  messages.add = function(message){
+    messages.list.push({id: messages.list.length, text: message});
+  };
+
+  return messages;
+});
+
+myAppModule.controller('ListCtrl', function(messages){
+	var self = this;
+
+	self.messages = messages.list;
+	
+})
+
+myAppModule.controller('PostCtrl', function(messages){
+	var self = this;
+	self.newMessage = 'hola';
+	self.addMessage = function(message){
+		messages.add(message);
+		self.newMessage='';
+	};
+
+
+})
