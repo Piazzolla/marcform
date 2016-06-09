ba00bcdaa57c41669df69a11aea91acb39c6d422
Mariano Burgos <mariano.burgos@pjn.gov.ar>
2016-06-09 15:45:31 -0300

    agrego angular material, lo aplico a algunos elementos del html y agrego un ejemplo de select con autocomplete funcionando para reemplazar los selects existentes

--- MarcForm/app/app.js
+++ MarcForm/app/app.js
@@ -1,9 +1,25 @@
 // declare a module
 (function() {
-var myAppModule = angular.module('marcform', [])
+var myAppModule = angular.module('marcform', ['ngMaterial'])
 .controller('MainCtrl', ['$scope', 'DiccionarioService', function ($scope, DiccionarioService)  {
 	$scope.codigosMarc = DiccionarioService.getCodigosMarc();
 	$scope.subcodigosMarc = DiccionarioService.getSubcodigosMarc();
+
+
+$scope.colours = [
+    { name: 'black', id: 0 },
+    { name: 'white', id: 1 },
+    { name: 'red',   id: 2 }
+    ];
+
+$scope.selectedColour = $scope.colours[2]; // red.
+
+$scope.colourChanged = function (value) {
+    var colourName = value ? value.name : "none";
+    $scope.message = "ac-change event fired for colour. New colour: " + colourName;
+    $scope.$digest();
+};
+
 //	$scope.campos = [];
 //	$scope.campos.subcampos = [];
 }])
@@ -59,9 +75,9 @@ myAppModule.controller('debugCtrl', function($scope){
 myAppModule.controller('getJsonCtrl', function($scope, $http) {
 	$scope.getJson = function(){
 		$http.get('../data/template.json',  {headers: { 'Accept': 'application/json' }})
-		.then(function(res){
+		.then(function( ){
 			$scope.template = res.data;
-	//		$scope.campos = $scope.template;
+//			$scope.campos = $scope.template;
 			console.log("Template length: " + $scope.template.length);
 			for(var i=0; i < $scope.template.length; i++){
             	console.log("Dato número " + i +": " + $scope.template[i].dato.codigo);
@@ -100,5 +116,76 @@ myAppModule.controller('ctrl', function ($scope) {
     $scope.scrollTo = function (target){
     };
 });
+
+
+
+/*********************************************************************/
+myAppModule.controller('DemoCtrl', DemoCtrl);
+  function DemoCtrl ($timeout, $q, $log) {
+    var self = this;
+    self.simulateQuery = false;
+    self.isDisabled    = false;
+    // list of `state` value/display objects
+    self.states        = loadAll();
+    self.querySearch   = querySearch;
+    self.selectedItemChange = selectedItemChange;
+    self.searchTextChange   = searchTextChange;
+    self.newState = newState;
+    function newState(state) {
+      alert("Sorry! You'll need to create a Constituion for " + state + " first!");
+    }
+    // ******************************
+    // Internal methods
+    // ******************************
+    /**
+     * Search for states... use $timeout to simulate
+     * remote dataservice call.
+     */
+    function querySearch (query) {
+      var results = query ? self.states.filter( createFilterFor(query) ) : self.states,
+          deferred;
+      if (self.simulateQuery) {
+        deferred = $q.defer();
+        $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
+        return deferred.promise;
+      } else {
+        return results;
+      }
+    }
+    function searchTextChange(text) {
+      $log.info('Text changed to ' + text);
+    }
+    function selectedItemChange(item) {
+      $log.info('Item changed to ' + JSON.stringify(item));
+    }
+    /**
+     * Build `states` list of key/value pairs
+     */
+    function loadAll() {
+      var allStates = 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware,\
+              Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana,\
+              Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana,\
+              Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina,\
+              North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina,\
+              South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia,\
+              Wisconsin, Wyoming';
+      return allStates.split(/, +/g).map( function (state) {
+        return {
+          value: state.toLowerCase(),
+          display: state
+        };
+      });
+    }
+    /**
+     * Create filter function for a query string
+     */
+    function createFilterFor(query) {
+      var lowercaseQuery = angular.lowercase(query);
+      return function filterFn(state) {
+        return (state.value.indexOf(lowercaseQuery) === 0);
+      };
+    }
+  }
+
 
 })();
